d4f29c570fe1ede279360fac51df426f4f67aaa9
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-22 12:08:19 +0900

    Use ENV values for rendering views

---
+++ helloworld/hello_amznpay.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+before do
+  @client_id = ENV.fetch('YOUR_CLIENT_ID')
+  @seller_id = ENV.fetch('YOUR_MERCHANT_ID')
+end
+
+get '/' do
+  erb :index
+end
