39c5969fa449efdeb41a8524a6dff187a2080a41
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-23 00:16:51 +0900

    Add required params in client initialize

--- helloworld/hello_amznpay.rb
+++ helloworld/hello_amznpay.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'amazon_pay'
 
 before do
-  @client_id = ENV.fetch('YOUR_CLIENT_ID')
-  @seller_id = ENV.fetch('YOUR_MERCHANT_ID')
+  @client_id   = ENV.fetch('YOUR_CLIENT_ID')
+  @seller_id   = ENV.fetch('YOUR_MERCHANT_ID')
+  @merchant_id = ENV.fetch('YOUR_MERCHANT_ID')
+  @access_key  = ENV.fetch('YOUR_ACCESS_KEY')
+  @secret_key  = ENV.fetch('YOUR_SECRET_KEY')
 end
 
 get '/' do
@@ -16,17 +18,17 @@ get '/redirected' do
 end
 
 post '/pay' do
-  merchant_id = ENV.fetch('YOUR_MERCHANT_ID')
-  access_key  = ENV.fetch('YOUR_ACCESS_KEY')
-  secret_key  = ENV.fetch('YOUR_SECRET_KEY')
-
+  require 'amazon_pay'
+  erb :pay
+  # Step4 Add Order Detail
   client = AmazonPay::Client.new(
-      merchant_id,
-      access_key,
-      secret_key,
+      @merchant_id,
+      @access_key,
+      @secret_key,
+      region: :jp, # :jp 指定しないと deny される
+      currency_code: :jpy, # :jpy 指定しないと deny される
       sandbox: true,
-      currency_code: :usd,
-      region: :na
+      log_enabled: true
   )
 
   amazon_order_reference_id = params[:orderReferenceId]
@@ -41,8 +43,17 @@ post '/pay' do
       store_name: 'CourtAndCherry.com'
   )
 
-  # Making a call to the ConfirmOrderReference API
+  # # Making a call to the ConfirmOrderReference API
   client.confirm_order_reference(amazon_order_reference_id)
 
-  redirect '/'
+  # # Step5 Open Auth
+  authorization_reference_id = 'test_authorize_' + Time.now.to_s
+
+  client.authorize(
+      amazon_order_reference_id,
+      authorization_reference_id,
+      amount,
+      seller_authorization_note: 'Lorem ipsum dolor'
+  )
+
 end
