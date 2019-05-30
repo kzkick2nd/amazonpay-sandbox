bb0721b999ff2d23724b4fa8f998c10e9f965b38
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-30 17:44:30 +0900

    Add AutoPay Step4,5,6

--- helloworld/hello_amznpay.rb
+++ helloworld/hello_amznpay.rb
@@ -115,5 +115,112 @@ get '/pay_auto' do
   erb :pay_auto
 end
 
-post '/pay_onetime' do
+post '/pay_auto' do
+  client = AmazonPay::Client.new(
+    @merchant_id,
+    @access_key,
+    @secret_key,
+    sandbox: true,
+    region: :jp, # :jp 指定しないと deny される
+    currency_code: :jpy, # :jpy 指定しないと deny される
+    log_enabled: false
+  )
+  # These values are grabbed from the Amazon Pay
+  # Address and Wallet widgets
+  amazon_billing_agreement_id = params[:billingAgreementId]
+  # address_consent_token = params[]
+
+  # To get the buyers full address if shipping/tax
+  # calculations are needed you can use the following
+  # API call to obtain the billing agreement details.
+  client.get_billing_agreement_details(
+    amazon_billing_agreement_id
+    # address_consent_token
+  )
+
+  # Next you will need to set the various details
+  # for this subscription with the following API call.
+  # There are additional optional parameters that
+  # are not used below.
+  client.set_billing_agreement_details(
+    amazon_billing_agreement_id,
+    seller_note: 'Your Seller Note',
+    seller_billing_agreement_id: 'Your Transaction Id',
+    store_name: 'Your Store Name',
+    custom_information: 'Additional Information'
+  )
+
+  # Make the ConfirmBillingAgreement API call to confirm
+  # the Amazon Billing Agreement Id with the details set above.
+  # Be sure that everything is set correctly above before
+  # confirming.
+  client.confirm_billing_agreement(
+    amazon_billing_agreement_id
+  )
+
+  # The following API call is not needed at this point, but
+  # can be used in the future when you need to validate that
+  # the payment method is still valid with the associated billing
+  # agreement id.
+  client.validate_billing_agreement(
+    amazon_billing_agreement_id
+  )
+
+  # Set the amount for your first authorization.
+  amount = '10.00'
+
+  # Set a unique authorization reference id for your
+  # first transaction on the billing agreement.
+  authorization_reference_id = 'bill_ref_id_' + Time.now.to_i.to_s
+
+  # Now you can authorize your first transaction on the
+  # billing agreement id. Every month you can make the
+  # same API call to continue charging your buyer
+  # with the 'capture_now' parameter set to true. You can
+  # also make the Capture API call separately. There are
+  # additional optional parameters that are not used
+  # below.
+
+  # NOTE res が入ってないぞ => PR 出
+  res = client.authorize_on_billing_agreement(
+    amazon_billing_agreement_id,
+    authorization_reference_id,
+    amount,
+    currency_code: 'JPY', # :jpy 指定しないと deny される
+    seller_authorization_note: 'Your Authorization Note',
+    transaction_timeout: 0, # Set to 0 for synchronous mode
+    capture_now: true, # Set this to true if you want to capture the amount in the same API call
+    seller_note: 'Your Seller Note',
+    seller_order_id: 'Your Order Id',
+    store_name: 'Your Store Name',
+    custom_information: 'Additional Information'
+  )
+
+  # You will need the Amazon Authorization Id from the
+  # AuthorizeOnBillingAgreement API response if you decide
+  # to make the Capture API call separately.
+  amazon_authorization_id = res.get_element('AuthorizeOnBillingAgreementResponse/AuthorizeOnBillingAgreementResult/AuthorizationDetails','AmazonAuthorizationId')
+
+  # Set a unique id for your current capture of
+  # this payment.
+  capture_reference_id = 'Your Unique Id'
+
+  # Make the Capture API call if you did not set the
+  # 'capture_now' parameter to 'true'. There are
+  # additional optional parameters that are not used
+  # below.
+  client.capture(
+    amazon_authorization_id,
+    capture_reference_id,
+    amount,
+    currency_code: 'USD', # Default: USD
+    seller_capture_note: 'Your Capture Note'
+  )
+
+  # The following API call should not be made until you
+  # are ready to terminate the billing agreement.
+  client.close_billing_agreement(
+    amazon_billing_agreement_id,
+    closure_reason: 'Reason For Closing'
+  )
 end
